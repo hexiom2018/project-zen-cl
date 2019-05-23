e108aef44c179adf11399abd7da27d39a7cd846b
Syed UZair Ali <maazuzair3322@gmail.com>
2019-05-23 20:48:04 +0500

    aaad

---
+++ src/app/navigations/MainNavigation.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+
+import firebase from "firebase";
+
+import { createStackNavigator, createAppContainer } from "react-navigation";
+import { SignIn, Dashboard, Splash, SignUp } from "../screens";
+
+var config = {
+  apiKey: "AIzaSyDTO_VW8KigcpywWHw8q10DCQBRz0uBW54",
+  authDomain: "zenclause.firebaseapp.com",
+  databaseURL: "https://zenclause.firebaseio.com",
+  projectId: "zenclause",
+  storageBucket: "zenclause.appspot.com",
+  messagingSenderId: "284077417988"
+};
+
+firebase.initializeApp(config);
+
+const App = createStackNavigator({
+  Dashboard: { screen: Dashboard },
+  SignIn: { screen: SignIn },
+  Splash: { screen: Splash },
+  SignUp: { screen: SignUp }
+});
+
+// const App = createStackNavigator({
+//   SignIn: { screen: SignIn },
+//   Dashboard: { screen: Dashboard },
+//   Splash: { screen: Splash },
+//   SignUp: { screen: SignUp }
+// });
+
+
+export const MainNavigation = createAppContainer(App);
+
+console.disableYellowBox = true;
